60c764797c0ae16f3c743e2f48bfb1247ce0eb1e
a.shigeru <a.shigeru@gmail.com>
2014-06-25 19:44:07 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+/*jslint node:true, nomen:true, plusplus:true, unparam:true*/
+(function () {
+    'use strict';
+
+    var scraper = require('scraper'),
+        async   = require('async'),
+        _       = require('underscore'),
+        async_stack = [],
+        timeout = 500; //(ms)
+
+    _([
+        'http://example.com/',
+    ]).each(function (url, i, urls) {
+
+        var process = function (callback) {
+
+            console.log('# url:', url);
+
+            scraper(url, function (err, $) {
+                if (err) { throw err; }
+
+                $('[onclick]').each(function (i, element) {
+                    var click = $(element).attr('onclick');
+
+                    console.log('    ', click);
+                });
+
+                setTimeout(function () {
+                    callback();
+                }, timeout);
+            });
+        };
+
+        async_stack.push(process);
+    });
+
+    async.series(async_stack);
+}());
